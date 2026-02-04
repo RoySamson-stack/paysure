b5f0d48de5a3b8f733d116b350a8b336bd3ecd7f
RoySamson-stack <rsams9009@gmail.com>
2026-02-04 14:46:33 +0300

    Add PWA support: installable web app as alternative to APK

---
+++ paysure-website/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'msalary-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
